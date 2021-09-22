6fd3dad6e76616a4ef9b79dd229928f4103b9e4d
Salmaan <salmaan.arif.ali@gmail.com>
2021-09-21 23:50:09 -0400

    write actions for fetching courses and teachers

---
+++ frontend/src/actions/courseActions.js
@@ -0,0 +1,8 @@
+export const fetchCourses = () => {
+  return (dispatch) => {
+    dispatch({type: "LOADING_COURSES"})
+    fetch("http://localhost:9999/courses")
+      .then( resp => resp.json() )
+      .then( courses => dispatch({type: "GET_COURSES", courses: courses}))
+  }
+}
