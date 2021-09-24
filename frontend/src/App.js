146b4da34c7ca1d665e8de9e3e9360911be5827c
Salmaan <salmaan.arif.ali@gmail.com>
2021-09-24 16:33:38 -0400

    reformat code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import NavBar from './components/NavBar.js'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import NavBar from './components/NavBar.js'
 import Home from './components/Home.js'
 import Dashboard from './components/Dashboard.js'
 import Catalog from './components/Catalog.js'
