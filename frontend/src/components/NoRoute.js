2dd2ad728a3454d567a2d74ceb7207ff41378d22
Salmaan <salmaan.arif.ali@gmail.com>
2021-09-24 16:08:36 -0400

    update styling

--- frontend/src/components/NoRoute.js
+++ frontend/src/components/NoRoute.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './NoRoute.css'
 
 const NoRoute = () => {
   return (
-    <div>
+    <div id="no-route">
       Sorry, this URL doesn't exist. Please use one of the above navigation bar options to get back on track.
     </div>
   )
