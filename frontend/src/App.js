b7bd3eefa4335bb8861041375455b555531f3461
Salmaan <salmaan.arif.ali@gmail.com>
2021-09-24 15:50:33 -0400

    remove navbar container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import NavBarContainer from './containers/NavBarContainer'
+import NavBar from './components/NavBar.js'
+
 
 function App() {
   return (
     <div className='App'>
-      <NavBarContainer />
+      <NavBar />
     </div>
   );
 }
