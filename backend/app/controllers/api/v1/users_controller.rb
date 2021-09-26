53ac80a9f26d38b93e2dd3e209452cb01447a482
Salmaan <salmaan.arif.ali@gmail.com>
2021-09-26 11:58:11 -0400

    Added post feature to Users

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: { user: UserSeralizer.new(@user) }, status: :created
+      render json: { user: UserSerializer.new(@user) }, status: :created
     else
-      render json: { error: 'failed to create user' }, status: :unprocessable_entity
+      render json: { error: 'failed to create user', params: params }, status: :unprocessable_entity
     end
   end
 
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :user, only: [:create]
+      resources :users, only: [:create]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
     end
