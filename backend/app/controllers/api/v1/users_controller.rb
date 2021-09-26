074a05a4681022bf3794d6879165435525a88d0a
Salmaan <salmaan.arif.ali@gmail.com>
2021-09-25 23:15:35 -0400

    set up route actions

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: UserSeralizer.new(@user) }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/courses', to: 'course#index'
-  get '/courses/:id', to: 'course#show'
-  get '/teachers', to: 'teacher#index'
-  get '/teachers/:id', to: "teacher#show"
+  namespace :api do
+    namespace :v1 do
+      resources :user, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
 end
