22c923a585d5943560cb19c9a52fb23fbe3aca32
Salmaan <salmaan.arif.ali@gmail.com>
2021-09-28 19:44:49 -0400

    fix typo

--- backend/app/controllers/api/v1/auth_controller.rb
+++ backend/app/controllers/api/v1/auth_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::AuthController < ApplicationController
     @user = User.find_by(email: user_login_params[:email])
     if @user && @user.authenticate(user_login_params[:password])
       token = encode_token( {user_id: @user.id} )
-      render json: { user: UserSerializer.new(@user), jwt: token }, status: :acceptede
+      render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
     else
       render json: {message: "Invalid email or password"}, status: :unauthorized
     end
