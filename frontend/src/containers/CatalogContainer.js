50de3b1d8f528feae7363c61289a5c77f6b55b54
Salmaan <salmaan.arif.ali@gmail.com>
2021-09-28 21:34:30 -0400

    map state to props

--- frontend/src/containers/CatalogContainer.js
+++ frontend/src/containers/CatalogContainer.js
@@ -1,20 +1,22 @@
 import React, {Component} from "react";
 import CourseCard from "../components/CourseCard";
+import {connect} from 'react-redux'
 
 class CatalogContainer extends Component {
   renderAllCourses() {
     // rendering dummy data
-    const dummyData = [
-      {id: 1, courseId: "MATH301", title: "Mathemetics"},
-      {id: 2, courseId: "ENG412", title: "English"},
-      {id: 3, courseId: "CHEM101", title: "Chemistry"},
-      {id: 4, courseId: "MATH301", title: "Mathemetics"},
-      {id: 5, courseId: "ENG412", title: "English"},
-      {id: 6, courseId: "CHEM101", title: "Chemistry"}
-    ]
-
+    // const dummyData = [
+    //   {id: 1, courseId: "MATH301", title: "Mathemetics"},
+    //   {id: 2, courseId: "ENG412", title: "English"},
+    //   {id: 3, courseId: "CHEM101", title: "Chemistry"},
+    //   {id: 4, courseId: "MATH301", title: "Mathemetics"},
+    //   {id: 5, courseId: "ENG412", title: "English"},
+    //   {id: 6, courseId: "CHEM101", title: "Chemistry"}
+    // ]
+    const courses = this.props.courses
+    debugger;
     return (
-      dummyData.map(courseInfo => <CourseCard courseInfo={courseInfo} key={courseInfo.id} />)
+      courses.map(courseInfo => <CourseCard courseInfo={courseInfo} key={courseInfo.id} />)
     )
   }
 
@@ -28,4 +30,8 @@ class CatalogContainer extends Component {
   }
 }
 
-export default CatalogContainer
+const mapStateToProps = (state) => (
+  {courses: state.courses}
+)
+
+export default connect(mapStateToProps)(CatalogContainer)
