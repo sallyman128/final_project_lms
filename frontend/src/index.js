4d1cb1299c4378f103331da9a7acc3f378ce1c28
Salmaan <salmaan.arif.ali@gmail.com>
2021-09-30 12:31:30 -0400

    link redux chrome extension

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import {Provider} from 'react-redux'
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import rootReducer from './reducers/allReducers'
 import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
