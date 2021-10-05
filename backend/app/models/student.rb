99464f8b3416a246dce14808070bed5c530605ff
Salmaan <salmaan.arif.ali@gmail.com>
2021-10-04 23:43:31 -0400

    validate student email for uniqueness

--- backend/app/models/student.rb
+++ backend/app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ApplicationRecord
+  validates :email, uniqueness: {case_sensitive: false}
+
   has_many :student_courses
   has_many :courses, through: :student_courses
   has_many :users, through: :courses
