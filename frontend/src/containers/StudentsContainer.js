78541a7f658a81d2063191d678a903cc4308f898
Salmaan <salmaan.arif.ali@gmail.com>
2021-11-15 19:07:24 -0500

    add filter feature

--- frontend/src/containers/StudentsContainer.js
+++ frontend/src/containers/StudentsContainer.js
@@ -3,11 +3,45 @@ import { connect } from 'react-redux'
 import StudentsTable from '../components/StudentsTable'
 
 class StudentsContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      students: []
+    }
+  }
+
+  getStudents = () => {
+    this.setState( {
+      students: this.props.students
+    })
+  }
+
+  searchStudents = () => {
+    const search = document.getElementById('studentSearchWord').value
+    let students =[]
+
+    if (search === "") {
+      students = this.props.students
+    } else {
+      students = this.props.students.filter(student => student.name.toLowerCase().includes(search) )
+    }
+    
+    this.setState({
+      students: students
+    })
+  }
+
+  componentDidMount = () => {
+    this.getStudents()
+  }
+
   render() {
     return(
       <div>
         <h1>All Students</h1>
-        <StudentsTable students={this.props.students}/>  
+        <input type='text' id='studentSearchWord'/>
+        <button id='studentSearchButton' onClick={() => this.searchStudents()}>Search</button>
+        <StudentsTable students={this.state.students}/>  
       </div>
     )
   }
