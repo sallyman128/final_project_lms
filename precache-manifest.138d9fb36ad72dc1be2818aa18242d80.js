ed41fffed275451fe70b21a36596bcf47c01e4e2
sallyman128 <salmaan.arif.ali@gmail.com>
2023-07-21 12:34:13 -0400

    Updates

---
+++ precache-manifest.138d9fb36ad72dc1be2818aa18242d80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4542e870f78de898824",
+    "url": "/final_project_lms/static/css/main.58ffb5b4.chunk.css"
+  },
+  {
+    "revision": "d4542e870f78de898824",
+    "url": "/final_project_lms/static/js/main.d4542e87.chunk.js"
+  },
+  {
+    "revision": "a379dfd70043674e13d2",
+    "url": "/final_project_lms/static/js/1.a379dfd7.chunk.js"
+  },
+  {
+    "revision": "d2aadfe71f5aeac167e1",
+    "url": "/final_project_lms/static/js/runtime~main.d2aadfe7.js"
+  },
+  {
+    "revision": "4b1fca449caeb5a76fab27c0ca7cadb0",
+    "url": "/final_project_lms/index.html"
+  }
+];
