aaa71a9a138640889bd7df9ac8a645452de492d4
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-07-23 13:47:37 -0400

    update content for tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx}",
+    "./public/index.html",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
