3eb56ce296050df6fd0ff4c8e4f4761a8bd1a1d3
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-07-23 14:09:03 -0400

    implement bootstrap

--- frontend/src/containers/HomeContainer.js
+++ frontend/src/containers/HomeContainer.js
@@ -3,8 +3,12 @@ import React, {Component} from "react";
 class HomeContainer extends Component {
   render() {
     return(
-      <div>
-        <h1>Welcome to MyLMS!</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1 className="text-center">Welcome to MyLMS!</h1>
+          </div>
+        </div>
       </div>
     )
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import {createStore, applyMiddleware, compose} from 'redux'
 import rootReducer from './reducers/allReducers'
 import {thunk} from 'redux-thunk'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
