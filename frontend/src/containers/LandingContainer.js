353155a90eddaebfb97f3e7994a7508e8dc5d4be
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-07-23 16:03:15 -0400

    fixed styling of login and signup buttons

--- frontend/src/containers/LandingContainer.js
+++ frontend/src/containers/LandingContainer.js
@@ -8,12 +8,12 @@ const LandingContainer = () => {
     <div className="vh-100 gradient-custom d-flex justify-content-center align-items-center">
       <div className="text-center text-white">
         <h1 className="display-4 fw-bold mb-5">My LMS</h1>
-        <div className="row d-flex justify-content-center">
-          <div className="col-6 col-md-3 mb-2">
-            <Link to="/signup" className="btn btn-outline-light btn-lg w-100">Sign Up</Link>
+        <div className="d-flex justify-content-center">
+          <div className="mx-2">
+            <Link to="/signup" className="btn btn-outline-light btn-lg">Sign Up</Link>
           </div>
-          <div className="col-6 col-md-3 mb-2">
-            <Link to="/login" className="btn btn-outline-light btn-lg w-100">Login</Link>
+          <div className="mx-2">
+            <Link to="/login" className="btn btn-outline-light btn-lg">Login</Link>
           </div>
         </div>
       </div>
