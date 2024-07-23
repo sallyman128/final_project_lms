a13760623708ea9b2f1d779ce09bbfbd3f8137d8
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-07-23 14:40:55 -0400

    updated signup form to use same styling as login form

--- frontend/src/components/SignupForm.js
+++ frontend/src/components/SignupForm.js
@@ -1,28 +1,48 @@
 import React from "react";
 import ErrorsList from "./ErrorsList";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const SignupForm = ({handleSubmit, handleChange, errors}) => {
+const SignupForm = ({ handleSubmit, handleChange, errors }) => {
   return (
-    <div id='signup-form'>
+    <form onSubmit={(e) => handleSubmit(e)}>
       <ErrorsList errors={errors} />
-
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <p>
-          Name:
-          <input type="string" id="name" onChange={(e) => handleChange(e)} />
-        </p>
-        <p>
-          Email: 
-          <input type="string" id="email" onChange={(e) => handleChange(e)} />
-        </p>
-        <p>
-          Password:
-          <input type="password" id="password" onChange={(e) => handleChange(e)} />
-        </p>
-        <button type="submit" value="Login">Submit</button>
-      </form>
-    </div>
-  )
+      <div className="form-outline form-white mb-4">
+        <input 
+          type="text" 
+          id="name" 
+          className="form-control form-control-lg" 
+          onChange={(e) => handleChange(e)} 
+        />
+        <label className="form-label" htmlFor="name">Name</label>
+      </div>
+      <div className="form-outline form-white mb-4">
+        <input 
+          type="email" 
+          id="email" 
+          className="form-control form-control-lg" 
+          onChange={(e) => handleChange(e)} 
+        />
+        <label className="form-label" htmlFor="email">Email</label>
+      </div>
+      <div className="form-outline form-white mb-4">
+        <input 
+          type="password" 
+          id="password" 
+          className="form-control form-control-lg" 
+          onChange={(e) => handleChange(e)} 
+        />
+        <label className="form-label" htmlFor="password">Password</label>
+      </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          {errors.map((error, index) => (
+            <p key={index}>{error}</p>
+          ))}
+        </div>
+      )}
+      <button className="btn btn-outline-light btn-lg px-5" type="submit">Sign Up</button>
+    </form>
+  );
 }
 
-export default SignupForm
+export default SignupForm;
--- frontend/src/containers/SignUpContainer.js
+++ frontend/src/containers/SignUpContainer.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {userActions} from '../actions/userActions'
-import validator from 'validator' 
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { userActions } from '../actions/userActions';
+import validator from 'validator';
 import SignupForm from '../components/SignupForm';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/Signup.css';  // Assuming you have additional styles in this file
 
 class SignUpContainer extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -20,7 +20,7 @@ class SignUpContainer extends Component {
   }
 
   handleOnChange = (e) => {
-    this.setState( (currentState) => {
+    this.setState((currentState) => {
       return {
         user: {
           ...currentState.user,
@@ -31,47 +31,63 @@ class SignUpContainer extends Component {
   }
 
   handleOnSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (this.validate()) {
-      console.log('submitting signup form')
-      this.props.signup(this.state.user)
+      console.log('submitting signup form');
+      this.props.signup(this.state.user);
     }
   }
 
   validate = () => {
-    let errors = []
-    let isValid = true
+    let errors = [];
+    let isValid = true;
 
-    if(!this.state.user.name) {
-      isValid = false
-      errors.push("Please enter your name.")
+    if (!this.state.user.name) {
+      isValid = false;
+      errors.push("Please enter your name.");
     }
   
-    if(!this.state.user.email || !validator.isEmail(this.state.user.email)) {
-      isValid = false
-      errors.push("Please enter a valid email address.")
+    if (!this.state.user.email || !validator.isEmail(this.state.user.email)) {
+      isValid = false;
+      errors.push("Please enter a valid email address.");
     }
 
-    if(!this.state.user.password) {
-      isValid = false
-      errors.push("Please enter your password.")
+    if (!this.state.user.password) {
+      isValid = false;
+      errors.push("Please enter your password.");
     }
 
-    this.setState({errors})
-    return isValid
+    this.setState({ errors });
+    return isValid;
   }
 
   render() {
     return (
-      <div>
-        <h1>Sign Up</h1>
-        <SignupForm 
-          handleSubmit={this.handleOnSubmit} 
-          handleChange={this.handleOnChange}
-          errors={this.state.errors}
-        />
-      </div>
-    )
+      <section className="vh-100 gradient-custom">
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div className="card bg-dark text-white" style={{ borderRadius: '1rem' }}>
+                <div className="card-body p-5 text-center">
+                  <div className="mb-md-5 mt-md-4 pb-5">
+                    <h2 className="fw-bold mb-2 text-uppercase">Sign Up</h2>
+                    <p className="text-white-50 mb-5">Please enter your details to sign up!</p>
+                    <SignupForm 
+                      handleSubmit={this.handleOnSubmit} 
+                      errors={this.state.errors} 
+                      handleChange={this.handleOnChange}
+                    />
+                  </div>
+                  <div>
+                    <p className="mb-0">Already have an account? <a href="/login" className="text-white-50 fw-bold">Login</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
   }
 }
 
@@ -81,4 +97,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default connect(null, mapDispatchToProps)(SignUpContainer)
+export default connect(null, mapDispatchToProps)(SignUpContainer);
