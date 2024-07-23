5e7d0741f83fe3aec49640274fa152f908649083
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-07-23 14:57:55 -0400

    updated styling of landing page

--- frontend/src/containers/HomeContainer.js
+++ frontend/src/containers/HomeContainer.js
@@ -1,17 +1,24 @@
-import React, {Component} from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/HomeContainer.css';
 
-class HomeContainer extends Component {
-  render() {
-    return(
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h1 className="text-center">Welcome to MyLMS!</h1>
+const HomeContainer = () => {
+  return (
+    <div className="vh-100 gradient-custom d-flex justify-content-center align-items-center">
+      <div className="text-center text-white">
+        <h1 className="display-4 fw-bold mb-5">My LMS</h1>
+        <div className="row d-flex justify-content-center">
+          <div className="col-6 col-md-3 mb-2">
+            <Link to="/signup" className="btn btn-outline-light btn-lg w-100">Sign Up</Link>
+          </div>
+          <div className="col-6 col-md-3 mb-2">
+            <Link to="/login" className="btn btn-outline-light btn-lg w-100">Login</Link>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  );
 }
 
-export default HomeContainer
+export default HomeContainer;
