a4057337ccda80fc654b5ce9e062ff98cbb72c1a
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-07-23 16:11:13 -0400

    updated styling of h2

--- frontend/src/containers/LandingContainer.js
+++ frontend/src/containers/LandingContainer.js
@@ -8,7 +8,7 @@ const LandingContainer = () => {
     <div className="vh-100 gradient-custom d-flex justify-content-center align-items-center">
       <div className="text-center text-white">
         <h1 className="display-4 fw-bold mb-5">Learning Management Software</h1>
-        <h2 className="display-6 fw-bold mb-5">Created by: Salmaan Ali</h2>
+        <h2 className="display-5 fw-bold mb-5">Created by: Salmaan Ali</h2>
         <div className="d-flex justify-content-center">
           <div className="mx-2">
             <Link to="/signup" className="btn btn-outline-light btn-lg">Sign Up</Link>
